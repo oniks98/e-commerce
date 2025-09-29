e36cf2ca29e7cfd082df5274d4266312e313a404
Yurii <oniks9821@gmail.com>
2025-09-29 22:08:38 +0300

    fix(seo-text): add icons

--- src/components/shop/catalog/seo-text.tsx
+++ src/components/shop/catalog/seo-text.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import clsx from 'clsx';
 import {
   Accordion,
   AccordionContent,
@@ -12,52 +12,58 @@ import { ArrowDownIcon, CheckIcon } from '@/lib/shop/icons';
 import Shadow from '@/components/ui/shadow';
 
 const SeoText = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
-
   return (
-    <div className="relativeflex mb-8 justify-items-end py-8">
+    <div className="relative mb-8 flex justify-center py-8">
       <div className="max-w-240">
-        <h2 className="text-grey mb-6 text-center text-3xl font-semibold">
+        <h2 className="text-dark mb-6 text-center text-3xl font-semibold">
           {seoText.title}
         </h2>
-        <div
-          className={`relative overflow-hidden transition-all duration-500 ${isExpanded ? 'max-h-full' : 'max-h-96'}`}
-        >
-          <div>
-            {seoText.paragraphs.map((p, i) => (
-              <p key={i} className="text-grey mb-4">
-                {p}
-              </p>
-            ))}
-            <h3 className="text-grey mt-6 mb-4 text-xl font-semibold">
-              {seoText.list.title}
-            </h3>
-            <ul>
-              {seoText.list.items.map((item, i) => (
-                <li key={i} className="mb-2 flex items-center">
-                  <CheckIcon className="mr-2 h-6 w-6 text-green-500" />
-                  <span>{item}</span>
-                </li>
-              ))}
-            </ul>
-            <p className="text-grey mt-4">{seoText.additionalText}</p>
-          </div>
-          {!isExpanded && (
-            <div className="from-light absolute bottom-0 left-0 h-32 w-full bg-gradient-to-t to-transparent"></div>
-          )}
-        </div>
-        <div className="mt-4 flex justify-center">
-          <button
-            onClick={() => setIsExpanded(!isExpanded)}
-            className="text-yellow-dark flex items-center text-lg"
-          >
-            <div
-              className={`transform transition-transform ${isExpanded ? 'rotate-180' : ''}`}
-            >
-              <ArrowDownIcon />
+
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1" className="group relative border-none">
+            <div className="relative">
+              <div
+                className={clsx(
+                  'prose text-dark relative max-w-none',
+                  'line-clamp-[10] group-data-[state=open]:line-clamp-none',
+                )}
+              >
+                {seoText.paragraphs.map((p, i) => (
+                  <p key={i} className="mb-4">
+                    {p}
+                  </p>
+                ))}
+                <h3 className="text-dark mt-6 mb-4 text-xl font-semibold">
+                  {seoText.list.title}
+                </h3>
+                <ul>
+                  {seoText.list.items.map((item, i) => (
+                    <li key={i} className="mb-2 flex items-center">
+                      <CheckIcon className="mr-2 h-6 w-6 text-green-500" />
+                      <span>{item}</span>
+                    </li>
+                  ))}
+                </ul>
+                <p className="text-dark mt-4">{seoText.additionalText}</p>
+              </div>
+
+              <div
+                className={clsx(
+                  'pointer-events-none absolute bottom-0 left-0 h-32 w-full bg-gradient-to-t from-white to-transparent transition-opacity',
+                  'group-data-[state=open]:opacity-0',
+                )}
+              />
             </div>
-          </button>
-        </div>
+
+            <AccordionContent className="hidden" />
+
+            <AccordionTrigger className="flex w-full items-center justify-center pt-4 [&>svg]:hidden [&[data-state=open]>div]:rotate-180">
+              <Shadow>
+                <ArrowDownIcon className="text-yellow h-[50px] w-[50px]" />
+              </Shadow>
+            </AccordionTrigger>
+          </AccordionItem>
+        </Accordion>
       </div>
     </div>
   );

4883dd63f45c7e45c61079b01459127d5f7d9e07
Yurii <oniks9821@gmail.com>
2025-10-01 15:55:58 +0300

    fix(subcategories): styles grid-cols

--- src/components/shop/catalog/subcategories.tsx
+++ src/components/shop/catalog/subcategories.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import clsx from 'clsx';
 
 import { CategoryTreeItem } from '@/lib/shop/actions/category';
+import { ArrowUpRightIcon } from '@/lib/shop/icons';
 import AngleDoubleUpIcon from '@/lib/shop/icons/angle-double-up-icon';
 import { getPlaceholder } from '@/lib/shop/media/cloudinary';
 
@@ -34,7 +35,7 @@ const Subcategories = ({
 
   return (
     <section className="py-8">
-      <div className="mb-8 flex items-center gap-3">
+      <div className="mb-3 flex items-center gap-3 pr-5 pl-5">
         <h1 className="text-dark text-3xl font-semibold">{categoryName}</h1>
         <div className="text-grey text-lg">{totalProducts} товарів</div>
       </div>
@@ -47,25 +48,44 @@ const Subcategories = ({
             : 'max-h-[2000px] opacity-100',
         )}
       >
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5">
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(270px,1fr))] gap-7 p-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {subcategories.map((subcategory) => (
-            <Link
-              key={subcategory.id}
-              href={`/catalog/${subcategory.slug}`}
-              className="group relative block overflow-hidden rounded-lg"
-            >
-              <div className="bg-yellow absolute top-0 left-1/2 h-1.5 w-5 -translate-x-1/2 scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></div>
-              <Image
-                src={getPlaceholder('category', subcategory.id)}
-                alt={subcategory.name}
-                width={250}
-                height={180}
-                className="h-full w-full object-cover"
-              />
-              <div className="bg-opacity-20 bg-dark absolute inset-0 flex items-start justify-start p-4">
-                <h3 className="pt-2 pl-2 text-left text-lg font-semibold text-white">
-                  {subcategory.name}
-                </h3>
+            <Link key={subcategory.id} href={`/catalog/${subcategory.slug}`}>
+              <div className="group relative h-[240px] w-full overflow-hidden rounded-lg bg-white shadow-lg transition-transform duration-300 hover:scale-105 md:h-[300px]">
+                <div
+                  className="absolute inset-0 bg-cover bg-center transition-transform duration-300 group-hover:scale-110"
+                  style={{
+                    backgroundImage: `url(${getPlaceholder(
+                      'category',
+                      subcategory.id,
+                    )})`,
+                  }}
+                />
+                <div className="bg-opacity-20 bg-light absolute inset-0" />
+
+                {/* Контент карточки */}
+                <div className="text-dark relative z-10 flex h-full flex-col p-4 md:p-5">
+                  {/* Верхняя часть с названием */}
+                  <div className="flex items-start justify-between">
+                    <h3 className="text-xl leading-[1.4] font-semibold">
+                      {subcategory.name}
+                    </h3>
+                  </div>
+
+                  {/* Нижняя часть с иконкой */}
+                  <div className="mt-auto flex justify-end">
+                    <div
+                      className={clsx(
+                        'border-yellow flex items-center justify-center rounded-full border-2',
+                        'h-10 w-10 md:h-12 md:w-12',
+                      )}
+                    >
+                      <ArrowUpRightIcon
+                        className={clsx('text-yellow h-3 w-3 md:h-4 md:w-4')}
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
             </Link>
           ))}

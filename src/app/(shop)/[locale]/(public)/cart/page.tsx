d52dded8c445f571b9cebfe8db4eef3c94d2d5ea
Yurii <oniks9821@gmail.com>
2025-10-06 22:41:46 +0300

    feat(structure): add cart-page and cart-component

--- src/app/(shop)/[locale]/(public)/cart/page.tsx
+++ src/app/(shop)/[locale]/(public)/cart/page.tsx
@@ -1,6 +1,11 @@
-'use client';
 import React from 'react';
 
-export default function Page() {
-  return <div>Page</div>;
+export default function CartPage() {
+  return (
+    <div>
+      src\components\shop\ui\breadcrumbs.tsx
+      src\components\shop\cart\execution-contract.tsx
+      src\components\shop\ui\advantages.tsx
+    </div>
+  );
 }
---
+++ src/components/shop/cart/execution-contract.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ExecutionContract = () => {
+  return (
+    <div>
+      src\components\shop\cart\buyer-info.tsx
+      src\components\shop\cart\delivery-select.tsx
+      src\components\shop\cart\payment-select.tsx
+      src\components\shop\cart\comment.tsx src\components\shop\cart\cart.tsx
+    </div>
+  );
+};
+export default ExecutionContract;
